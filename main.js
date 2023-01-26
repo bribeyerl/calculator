c3594882f5f43703bf0b429136f23e710468b039
bribeyerl <briabeyerl@gmail.com>
2023-01-26 16:49:24 -0500

    Display number input on calc

--- main.js
+++ main.js
@@ -11,3 +11,36 @@ function updateDisplay() {
 }
 
 updateDisplay()
+
+function inputDigit(digit) {
+    const { displayValue } = calc
+    calc.displayValue = displayValue === '0' ? digit : displayValue + digit
+}
+
+const keys = document.querySelector('.pad')
+keys.addEventListener('click', (event) => {
+    const { target } = event
+
+    if (!target.matches('button')) {
+        return
+    }
+    if (target.classList.contains('operator')) {
+        console.log('operator', target.value)
+        return
+    }
+    if (target.classList.contains('decimal')) {
+        console.log('decimal', target.value)
+        return
+    }
+    if (target.classList.contains('ac')) {
+        console.log('all clear', target.value)
+        return
+    }
+    if (target.classList.contains('clear')) {
+        console.log('clear', target.value)
+        return
+    }
+    inputDigit(target.value)
+    updateDisplay()
+
+})
